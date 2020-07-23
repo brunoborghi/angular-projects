69d98d23d23a2b67a4a7963c6cdc92bf7923f15a
Bruno Borghi <bruno.borghi@gmail.com>
2020-07-23 20:08:40 -0300

    Decorators de métodos e propriedades - Lazy loading

--- typescript-1/alurabank/app/ts/views/View.ts
+++ typescript-1/alurabank/app/ts/views/View.ts
@@ -1,3 +1,4 @@
+import {logarTempoDeExecucao} from '../helpers/decorators/index';
 export abstract class View<T> {
     private _elemento: JQuery;
     private _escapar: boolean;
@@ -7,6 +8,7 @@ export abstract class View<T> {
         this._escapar = escapar;
     }
 
+    @logarTempoDeExecucao(true)
     update(model: T) {
         let template = this.template(model);
         if (this._escapar)
